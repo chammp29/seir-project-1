d98fb88452624e18c44ed8abdbac1f8bccc817dd
Corey Hammond <coreyhammond@Coreys-Air.hitronhub.home>
2019-09-18 12:01:32 -0700

    bronze stage of game complete; can move disks to other columns, no rules or win logic

--- test.js
+++ test.js
@@ -4,17 +4,24 @@ const diskTwo = document.getElementById("disk-2");
 const diskThree = document.getElementById("disk-3");
 let targetDisk;
 
-diskOne.addEventListener("dragstart", dragStart);
-diskTwo.addEventListener("dragstart", dragStart);
-diskThree.addEventListener("dragstart", dragStart);
+// diskOne.addEventListener("dragstart", dragStart);
+// diskTwo.addEventListener("dragstart", dragStart);
+// diskThree.addEventListener("dragstart", dragStart);
 function dragStart(evt) {
   console.log(evt);
   targetDisk = evt.target;
   console.log(targetDisk);
+  console.log(evt.path[1].children[1]);
+  //   diskTwo.setAttribute("draggable", true);
+}
+function dragEnd(evt) {
+  //   console.log(evt.path[1].children[1]);
 }
 
 // Loop through columns and add "drop" event listeners"
 for (const col of columns) {
+  col.addEventListener("dragstart", dragStart);
+  col.addEventListener("dragend", dragEnd);
   col.addEventListener("dragover", dragOver);
   col.addEventListener("dragenter", dragEnter);
   col.addEventListener("dragleave", dragLeave);
@@ -36,7 +43,7 @@ function dragLeave() {
 }
 function dragDrop(evt) {
   console.log(evt);
-  this.appendChild(targetDisk);
+  this.insertBefore(targetDisk, this.children[0]);
 
   // if evt contains no disks, then
 }
