d7e0651d4fe142700da4ab1812fb730d6b028627
Corey Hammond <coreyhammond@Coreys-Air.hitronhub.home>
2019-09-18 14:08:44 -0700

    added logic to prevent a bigger square from going on top of a smaller square

--- test.js
+++ test.js
@@ -1,22 +1,8 @@
 const columns = document.querySelectorAll(".col");
-const diskOne = document.getElementById("disk-1");
-const diskTwo = document.getElementById("disk-2");
-const diskThree = document.getElementById("disk-3");
+// const diskOne = document.getElementById("disk-1");
+// const diskTwo = document.getElementById("disk-2");
+// const diskThree = document.getElementById("disk-3");
 let targetDisk;
-
-// diskOne.addEventListener("dragstart", dragStart);
-// diskTwo.addEventListener("dragstart", dragStart);
-// diskThree.addEventListener("dragstart", dragStart);
-function dragStart(evt) {
-  console.log(evt);
-  targetDisk = evt.target;
-  console.log(targetDisk);
-  console.log(evt.path[1].children[1]);
-  //   diskTwo.setAttribute("draggable", true);
-}
-function dragEnd(evt) {
-  //   console.log(evt.path[1].children[1]);
-}
 
 // Loop through columns and add "drop" event listeners"
 for (const col of columns) {
@@ -26,6 +12,19 @@ for (const col of columns) {
   col.addEventListener("dragenter", dragEnter);
   col.addEventListener("dragleave", dragLeave);
   col.addEventListener("drop", dragDrop);
+}
+
+function dragStart(evt) {
+  console.log(evt);
+  targetDisk = evt.target;
+  console.log(targetDisk);
+  console.log(evt.path[1].children[1]);
+  //   diskTwo.setAttribute("draggable", true);
+}
+function dragEnd(evt) {
+  // the dragend event is happening after the drop event, and the target is the disk
+  console.log(evt);
+  console.log("1");
 }
 
 function dragOver(evt) {
@@ -42,8 +41,30 @@ function dragLeave() {
   console.log("leave");
 }
 function dragDrop(evt) {
+  // the drop event is happening before the dragend event, and the target is the new column
   console.log(evt);
-  this.insertBefore(targetDisk, this.children[0]);
+  console.log("2");
 
-  // if evt contains no disks, then
+  // test out compare function
+  if (evt.path[0].childElementCount > 0) {
+    if (compare(this.children)) {
+      this.insertBefore(targetDisk, this.children[0]);
+    } else {
+      console.log("sorry");
+    }
+  } else {
+    this.insertBefore(targetDisk, this.children[0]);
+  }
+}
+
+function compare(childrenArray) {
+  if (targetDisk.clientWidth < childrenArray[0].clientWidth) {
+    return true;
+  } else {
+    return false;
+  }
+  //   for (let kids of childrenArray) {
+  //     let kidsWidth = kids.clientWidth;
+  //     console.log(kidsWidth);
+  //   }
 }
