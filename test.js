48f5188297e4232b5358f490321478f267cf29c8
Corey Hammond <coreyhammond@Coreys-Air.hitronhub.home>
2019-09-18 15:44:44 -0700

    add logic to check if the user wins

--- test.js
+++ test.js
@@ -1,92 +1,128 @@
+// Create variables
 const columns = document.querySelectorAll(".col");
 const diskOne = document.getElementById("disk-1");
 const diskTwo = document.getElementById("disk-2");
 const diskThree = document.getElementById("disk-3");
 const diskArray = [diskOne, diskTwo, diskThree];
-for (disk of diskArray) {
-  disk.addEventListener("mousedown", setDraggable);
-}
 let targetDisk;
-
-// Loop through columns and add "drop" event listeners"
-for (const col of columns) {
-  col.addEventListener("dragstart", dragStart);
-  col.addEventListener("dragend", dragEnd);
-  col.addEventListener("dragover", dragOver);
-  col.addEventListener("dragenter", dragEnter);
-  col.addEventListener("dragleave", dragLeave);
-  col.addEventListener("drop", dragDrop);
-}
+let moveCounter = 0;
+let winCounter = 0;
 
 function setDraggable(evt) {
   // testing evt
-  console.log(evt);
-  console.log(evt.path[0].clientWidth);
+  //   console.log(evt);
+  //   console.log(evt.path[0].clientWidth);
 
   // if evt has a previousSibling, then compare sizes
   if (evt.target.previousElementSibling !== null) {
     if (
-      evt.path[0].clientWidth > evt.target.previousElementSibling.clientWidth
+      evt.path[0].clientWidth > evt.path[0].previousElementSibling.clientWidth
     ) {
-      console.log("too big");
+      // set this to non-draggable
       this.setAttribute("draggable", false);
     }
   } else {
+    // set to draggable
     this.setAttribute("draggable", true);
   }
 }
 
+// DRAG FUNCTIONS
+
+// on dragStart, set the targetDisk to the event target
 function dragStart(evt) {
-  console.log(evt);
+  //   console.log(evt);
   targetDisk = evt.target;
-  console.log(targetDisk);
-  console.log(evt.path[1].children[1]);
-  //   diskTwo.setAttribute("draggable", true);
+  //   console.log(targetDisk);
 }
+
 function dragEnd(evt) {
   // the dragend event is happening after the drop event, and the target is the disk
-  console.log(evt);
-  console.log("1");
+  //   console.log(evt);
+  checkWin(evt);
 }
 
 function dragOver(evt) {
   evt.preventDefault();
-  console.log("Over");
 }
 
 function dragEnter(evt) {
   evt.preventDefault();
-  console.log("Enter");
 }
 
-function dragLeave() {
-  console.log("leave");
-}
+function dragLeave() {}
+
 function dragDrop(evt) {
   // the drop event is happening before the dragend event, and the target is the new column
-  console.log(evt);
-  console.log("2");
+  //   console.log(evt);
 
-  // test out compare function
+  // if the column has another disk in it, then run the compare function; place the disk if appropriate
   if (evt.path[0].childElementCount > 0) {
     if (compare(this.children)) {
       this.insertBefore(targetDisk, this.children[0]);
     } else {
-      console.log("sorry");
+      alert("You can't put a larger disk on top of a smaller disk.");
     }
   } else {
     this.insertBefore(targetDisk, this.children[0]);
   }
 }
 
+// Compare sizes of disks to prevent a larger disk from being placed on a smaller disk; returns true is the targetDisk is smaller than another disk in the column
 function compare(childrenArray) {
   if (targetDisk.clientWidth < childrenArray[0].clientWidth) {
     return true;
   } else {
     return false;
   }
-  //   for (let kids of childrenArray) {
-  //     let kidsWidth = kids.clientWidth;
-  //     console.log(kidsWidth);
-  //   }
 }
+
+// Check for a win
+function checkWin(evt) {
+  if (evt.path[1].childElementCount === 3) {
+    setNonDraggable();
+    playAgain();
+  }
+}
+
+function setNonDraggable() {
+  for (disk of diskArray) {
+    disk.removeEventListener("mousedown", setDraggable);
+  }
+  diskOne.setAttribute("draggable", false);
+  diskTwo.setAttribute("draggable", false);
+  diskThree.setAttribute("draggable", false);
+}
+
+function playAgain() {
+  let userAction = prompt(
+    "You've won the game! Would you like to play again?",
+    "Yes or No?"
+  );
+
+  if (userAction === "yes") {
+    gameStart();
+    alert("Great! Now try going the opposite direction!");
+  } else {
+    alert("Goodbye");
+  }
+}
+
+function gameStart() {
+  // Loop through diskArray and add event listener
+  for (disk of diskArray) {
+    disk.addEventListener("mousedown", setDraggable);
+  }
+  // Loop through columns and add event listeners
+  for (const col of columns) {
+    col.addEventListener("dragstart", dragStart);
+    col.addEventListener("dragend", dragEnd);
+    col.addEventListener("dragover", dragOver);
+    col.addEventListener("dragenter", dragEnter);
+    col.addEventListener("dragleave", dragLeave);
+    col.addEventListener("drop", dragDrop);
+  }
+}
+
+// Start the game
+gameStart();
